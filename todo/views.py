b206881dcf44076a6fd655ff11089642a78699c9
pal772 <pal722998@gmail.com>
2023-02-11 11:44:04 +0000

    Add new todo file and todo list.html

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
